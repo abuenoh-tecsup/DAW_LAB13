4210fea7463d8ebbde13b7d59f3817d67aadccc2
abuenoh-tecsup <alvaro.bueno@tecsup.edu.pe>
2025-06-15 02:56:24 -0500

    Fixed routes

--- src/main/java/com/tecsup/demo/controller/TemplateController.java
+++ src/main/java/com/tecsup/demo/controller/TemplateController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/templates")
+@RequestMapping
 public class TemplateController {
 
     @GetMapping("/ingredientes-list")
